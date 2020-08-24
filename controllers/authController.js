976eb1d5fe806492038690f72357505b8a9fecbc
Jeremy Nevado <jeremynevado@gmail.com>
2020-08-24 13:42:48 -0400

    Finished implementing getAllTours protect function. Added passwordChangedAt function to the user schema so we can track when users change their passwords so when they change their password they must login again to gain access. Added error handling for JWTexpired and JWTverification errors so if the JWT is not verified or it is expired the user cannot login. Also added a check to see if the user has been deleted so the token associated with the account is no longer valid.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -14,10 +15,11 @@ exports.signup = catchAsync(async (req, res, next) => {
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
+        passwordConfirm: req.body.passwordConfirm,
+        passwordChangedAt: req.body.passwordChangedAt
     });
 
-    const token = jwt.sign(newUser._id);
+    const token = signToken(newUser._id);
 
     res.status(201).json({
         status: 'success',
@@ -53,23 +55,47 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.protect = catchAsync(async (req, res, next) => {
     // 1) Getting token and check if it's there
-    let token; 
+    let token;
     if (
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
         token = req.headers.authorization.split(' ')[1];
     }
-    console.log(token);
 
     if (!token) {
-        return next(new AppError('You are not logged in! Please login to get access.', 401))
+        return next(
+            new AppError(
+                'You are not logged in! Please login to get access.',
+                401
+            )
+        );
     }
     // 2) Validate the token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
     // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new AppError(
+                'The user belonging to the token no longer exists.',
+                401
+            )
+        );
+    }
 
     // 4) Check if user changed password after the token was issued
-
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError(
+                'User recently changed password! Please login again.',
+                401
+            )
+        );
+    }
+    
+    //Grant access to protected route
+    req.user = currentUser;
     next();
 });
