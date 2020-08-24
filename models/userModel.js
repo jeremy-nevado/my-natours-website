ee370f3e721e0ba038f43bfdac752f61cd2bf96a
Jeremy Nevado <jeremynevado@gmail.com>
2020-08-23 20:13:56 -0400

    Reformatted the userModel

--- models/userModel.js
+++ models/userModel.js
@@ -34,11 +34,15 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
+    // Only run this function if password was actually modified
     if (!this.isModified('password')) return next();
 
+    // Hash the password with cost of 12
     this.password = await bcrypt.hash(this.password, 12);
 
-    this.passwordConfirm = undefined
+    // Delete the passwordConfirm
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
