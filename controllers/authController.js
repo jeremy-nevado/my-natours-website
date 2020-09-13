bb2474330baa90d7f1decdc8c728540f5dd136c2
Jeremy Nevado <jeremynevado@gmail.com>
2020-09-13 13:56:48 -0400

    Updated authController and email to send out custom reset password emails with proper formatting.

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,7 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Email = require('../utils/email');
 
-const signToken = id => {
+const signToken = (id) => {
     return jwt.sign({ id: id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN,
     });
@@ -191,18 +191,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
 
     // 3) Send it to user's email
-    const resetURL = `${req.protocol}://${req.get(
-        'host',
-    )}/api/v1/users/resetPassword/${resetToken}`;
-
-    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\n If you didn't forget your password, please ignore this email!`;
-
     try {
-        // await Email ({
-            // email: user.email,
-            // subject: 'Your password reset token (valid for 10 min)',
-            // message,
-        // });
+        const resetURL = `${req.protocol}://${req.get(
+            'host',
+        )}/api/v1/users/resetPassword/${resetToken}`;
+        await new Email(user, resetURL).sendPasswordReset();
 
         res.status(200).json({
             status: 'success',
