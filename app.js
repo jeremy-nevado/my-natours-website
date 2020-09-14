cb16b5c5fedef165bedc96cd49082f1c27f67d25
Jeremy Nevado <jeremynevado@gmail.com>
2020-09-13 20:31:27 -0400

    Updated app.js with comments

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const viewRouter = require('./routes/viewRoutes');
 // Creating the express app
 const app = express();
 
+// Setup pug and template path
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
