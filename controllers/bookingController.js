e02ffc4a19a9a2da29782d1e4a23c30db9982506
Jeremy Nevado <jeremynevado@gmail.com>
2020-09-15 21:22:39 -0400

    Changed small bug in retrieving users in bookingControllers.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -51,9 +51,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
-    const user = await User.findOne({ email: session.customer_email }).id;
+    const user = (await User.findOne({ email: session.customer_email })).id;
     const price = session.amount_total / 100;
-    console.log(price, user, tour);
     await Booking.create({ tour, user, price });
 };
 
