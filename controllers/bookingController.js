b4bf59dcb91effcd69fe62541de32ff3f136914e
Jeremy Nevado <jeremynevado@gmail.com>
2020-09-15 21:18:53 -0400

    Updated to print price tour and user in createBookingCheckout for testing

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,6 +53,7 @@ const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
     const user = await User.findOne({ email: session.customer_email }).id;
     const price = session.amount_total / 100;
+    console.log(price, user, tour);
     await Booking.create({ tour, user, price });
 };
 
