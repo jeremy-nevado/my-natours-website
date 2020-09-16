e4792f5416eb4332bfb3f865a58954c5d4c72d59
Jeremy Nevado <jeremynevado@gmail.com>
2020-09-15 20:51:40 -0400

    Added body-parser package to fix issues potentially.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 
@@ -62,7 +63,11 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.post('/webhook-checkout', express.raw(), bookingController.webhookCheckout);
+app.post(
+    '/webhook-checkout',
+    bodyParser({ type: 'application/json' }),
+    bookingController.webhookCheckout,
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
